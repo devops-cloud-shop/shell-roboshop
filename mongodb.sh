c7d7d131e6c2d59db0303f6945cb894639817a71
Prav-Akula <pravs.akula@outlook.com>
2025-09-28 19:18:01 -0400

    mondb remote connections updated

--- mongodb.sh
+++ mongodb.sh
@@ -40,6 +40,12 @@ VALIDATE $? "Enable mongodb"
 systemctl start mongod &>>$LOG_FILE
 VALIDATE $? "start mongodb"
 
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE $? "Allowing remote connections to MongoDB"
+
+systemctl restart mongod &>>$LOG_FILE
+VALIDATE $? "Restarted MongoDB"
+
 
 
 
