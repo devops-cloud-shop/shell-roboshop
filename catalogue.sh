b6dd9e55bf9f1eb6366c41a87fd2caa20d978b84
Prav-Akula <pravs.akula@outlook.com>
2025-09-28 19:53:23 -0400

    catalogue-service

--- catalogue.sh
+++ catalogue.sh
@@ -83,7 +83,7 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing mongodb client"
 
 INDEX=$(mongosh mongodb.daws86s.fun --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX != 0 ]; then
+if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Load Catalogue Products"
 else
