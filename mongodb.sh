33a589f8b50ff4f205f730bda34aa3a06c7a6294
Prav-Akula <pravs.akula@outlook.com>
2025-09-28 11:10:52 -0400

    mongodb

---
+++ mongodb.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOG_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOG_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-roboshop/15-logs.log
+
+mkdir -p $LOG_FOLDER  # -p creates folder if not exists
+
+echo "Script started execution at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then #checking if the user is root or not
+    echo "ERROR:: Please execute the script with root priviledges"
+    exit 1
+fi
+
+VALIDATE(){                
+if [ $1 -ne 0 ]; then 
+    echo -e "ERROR:: $2 ... $R FAILURE $N" | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "$2... $G SUCCESS $N" | tee -a $LOG_FILE
+fi
+}
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE $? "Adding mongo.repo"
+
+dnf install mongodb-org -y &>>$LOG_FILE
+VALIDATE $? "Installing mongodb"
+
+systemctl enable mongod &>>$LOG_FILE
+VALIDATE $? "Enable mongodb"
+
+systemctl start mongod &>>$LOG_FILE
+VALIDATE $? "start mongodb"
+
+
+
+
